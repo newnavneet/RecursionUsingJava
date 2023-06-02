518d3dbd3680f969eef085e8a5ea07cf7275e6ff
Nav511 <125399571+Nav511@users.noreply.github.com>
2023-06-03 01:11:14 +0530

    initial

---
+++ src/CharactersofString.java
@@ -0,0 +1,23 @@
+public class CharactersofString {
+
+    public static void print(String  str){
+        if(str.length() == 0){
+            return;
+        }
+        else {
+            //System.out.println(str.charAt(0));
+           // print(str.substring(1));
+
+            // REVERSE
+            print(str.substring(1));
+            System.out.println(str.charAt(0));
+        }
+    }
+
+    public static void main(String[] args) {
+        print("code ");
+
+    }
+
+
+}
---
+++ src/Factorial.java
@@ -0,0 +1,22 @@
+public class Factorial {
+    public static int fact(int n ){
+        // Base Case
+         if(n==0){
+             return 1;
+         }
+         else {
+             return n*fact(n-1);  // recursive calls
+
+    }
+
+}
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
+
+    }
+
+
+
+}
